aa16c5b52a65f9a73903a52e6ede744fc0a7c288
unknown <jeferson.aristaque@gmail.com>
2022-03-22 18:16:24 -0300

    Criando pagina usando Bootstrap e JS com validação

---
+++ script.js
@@ -0,0 +1,37 @@
+let nome = window.document.getElementById('nome')
+let sugestao = document.querySelector('#sugestao')
+let nomeOk = false
+let sugestaoOK = false
+
+function validaNome() {
+    let txtNome = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txtNome.innerHTML = 'Nome Inválido'
+        txtNome.style.color = 'red'
+    } else {
+        txtNome.innerHTML = 'Nome Válido'
+        txtNome.style.color = 'green'
+        nomeOk = true
+    }
+}
+
+function validaSugestao() {
+    let txtSugestao = document.querySelector('#txtSugestao')
+
+    if (sugestao.value.length >= 5) {
+        txtSugestao.innerHTML = 'Texto é muito grande, digite no máximo 100 caracteres'
+        txtSugestao.style.color = 'red'
+        txtSugestao.style.display = 'block'
+    } else {
+        txtSugestao.style.display = 'none'
+        sugestaoOK = true
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && sugestaoOK == true) {
+        alert('Sugestão enviada com sucesso!')
+    } else {
+        alert('Preencha os dados corretamente para enviar sua sugestão!')
+    }
+}
